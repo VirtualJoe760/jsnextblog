f4640e51a2c57721f976393d6495bb113b3a9d80
Joey Sardella <josephsardella@gmail.com>
2024-09-03 11:23:24 -0700

    added trump elon post

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,8 @@ export default function Page() {
       <p className="mb-4">
         {`Hello I'm Joe, I'm a software engineer, entrepreneur, marketing mind with political opinions. I plan to use this blog to simply exprees my points of view, learning experiences & thoughts with the internet.
         I will be posting everything from code tutorials to thoughts on politics here so let me say it here first, I'm not trying to please everyone, this blog is really just for my own purposes. I will be using it as a journal, 
-        an escape, and notes for learning. I decided to create this blog going through code tutorials. Its one thing to code along on videos, Its another thing to document the experience as you go and take good notes. I've never been
-        one to have a photographic super genius memory. I've worked hard to learn everything I know and do my best to consider all opinions and paths forward. All this being said, if you are someone who is following this blog,
-        saw this on my developer portfolio, or on social media, I hope you enjoy my content here and that it benefits you. Thanks for stopping by!`}
+        an escape, and notes for learning. I decided to create this blog to journal going through code tutorials and my thoughts on politics. To those who are following this blog,
+        I hope you enjoy my content here and that it benefits you. Thanks for stopping by!`}
       </p>
       <div className="my-8">
         <BlogPosts />
