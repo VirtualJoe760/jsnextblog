b9bb46e5cb7125854243cfbca37a0a40365007a5
Joey Sardella <josephsardella@gmail.com>
2024-09-03 12:00:43 -0700

    removed disqus

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,6 +11,7 @@ export default function Page() {
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Joe's News</h1>
       <BlogPosts />
+      
     </section>
   )
 }
